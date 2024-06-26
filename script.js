2c38de7206db794e00cbea6aedf7212d377b14f4
Mwakanemela <102220067+Mwakanemela@users.noreply.github.com>
2024-06-26 09:12:06 +0000

    wow!, what an achievement made it without looking at the solution - I guess my JS skills are getting better

--- script.js
+++ script.js
@@ -41,33 +41,32 @@ const calculateBill = () => {
   // get bill from user input & convert it into a number
   let bill = Number(billTotalInput.value)
   
-  console.log(bill)
+  // console.log(bill)
   // get the tip from user & convert it into a percentage (divide by 100)
-  
+  let tip = Number(tipInput.value) / 100
 
   // get the total tip amount
-
+  let tipAmount = bill * tip
 
   // calculate the total (tip amount + bill)
-
-
+  let total = tipAmount + bill
   // calculate the per person total (total divided by number of people)
-
+  let perPersonTotal = total / numberOfPeople
 
   // update the perPersonTotal on DOM & show it to user
-
+  perPersonTotalDiv.innerText = `$${perPersonTotal.toFixed(2)}`
 }
 
 // ** Splits the bill between more people **
 const increasePeople = () => {
   // increment the amount of people
-
+  numberOfPeople += 1
 
   // update the DOM with the new number of people
-
+  numberOfPeopleDiv.innerText = numberOfPeople
 
   // calculate the bill based on the new number of people
-
+  calculateBill()
 }
 
 // ** Splits the bill between fewer people **
@@ -75,14 +74,16 @@ const decreasePeople = () => {
   // guard clause
   // if amount is 1 or less simply return
   // (a.k.a you can't decrease the number of people to 0 or negative!)
-
+  if (numberOfPeople <= 1) {
+    return
+  }
 
   // decrement the amount of people
-
+  numberOfPeople -= 1
 
   // update the DOM with the new number of people
-
+  numberOfPeopleDiv.innerText = numberOfPeople
 
   // calculate the bill based on the new number of people
-
+  calculateBill()
 }
